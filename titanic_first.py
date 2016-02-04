39c6802fa88678c77fec0211fd9f5db05095cbc6
SusmitaBasak <susmitabasak@Susmitas-MacBook-Pro.local>
2016-02-04 23:31:26 +0100

    added test file

---
+++ titanic_first.py
@@ -0,0 +1,52 @@
+import pandas
+from sklearn.linear_model.logistic import LogisticRegression
+from sklearn import cross_validation
+from sklearn.cross_validation import KFold
+
+
+titanic = pandas.read_csv("titanic_train.csv")
+titanic_test = pandas.read_csv("titanic_test.csv")
+
+titanic["Age"] = titanic["Age"].fillna(titanic["Age"].median())
+titanic.loc[titanic["Sex"] == "male", "Sex"] = 0
+titanic.loc[titanic["Sex"] == "female", "Sex"] = 1
+
+titanic["Embarked"] = titanic["Embarked"].fillna("S")
+titanic.loc[titanic["Embarked"] == "S", "Embarked"] = 0
+titanic.loc[titanic["Embarked"] == "C", "Embarked"] = 1
+titanic.loc[titanic["Embarked"] == "Q", "Embarked"] = 2
+
+titanic_test["Age"] = titanic_test["Age"].fillna(titanic["Age"].median())
+titanic_test["Fare"] = titanic_test["Fare"].fillna(titanic_test["Fare"].median())
+titanic_test.loc[titanic_test["Sex"] == "male", "Sex"] = 0 
+titanic_test.loc[titanic_test["Sex"] == "female", "Sex"] = 1
+titanic_test["Embarked"] = titanic_test["Embarked"].fillna("S")
+
+titanic_test.loc[titanic_test["Embarked"] == "S", "Embarked"] = 0
+titanic_test.loc[titanic_test["Embarked"] == "C", "Embarked"] = 1
+titanic_test.loc[titanic_test["Embarked"] == "Q", "Embarked"] = 2
+
+predictors = ["Pclass", "Sex", "Age", "SibSp", "Parch", "Fare", "Embarked"]
+
+# Initialize the algorithm class
+#alg = LogisticRegression(random_state=1)
+alg = LogisticRegression()
+
+# Compute the accuracy score for all the cross validation folds.  (much simpler than what we did before!)
+scores = cross_validation.cross_val_score(alg, titanic[predictors], titanic["Survived"], cv=3)
+# Take the mean of the scores (because we have one for each fold)
+print(scores.mean())
+
+
+# Train the algorithm using all the training data
+alg.fit(titanic[predictors], titanic["Survived"])
+
+# Make predictions using the test set.
+predictions = alg.predict(titanic_test[predictors])
+
+# Create a new dataframe with only the columns Kaggle wants from the dataset.
+submission = pandas.DataFrame({
+        "PassengerId": titanic_test["PassengerId"],
+        "Survived": predictions
+    })
+submission.to_csv("kaggle.csv", index=False)
